670669c2d45209668542ed1a0ec84f3fac8eeeb6
Truong <ltvtd54@gmail.com>
2024-04-06 08:46:53 +0700

    Done Change Password after Login

--- routes/auth.js
+++ routes/auth.js
@@ -199,4 +199,35 @@ router.post("/ResetPassword/:token", userValidator.checkPass(),async function (r
 
 });
 
+router.post('/ChangePassword', checkLogin, userValidator.checkPass() , async function (req, res, next) {
+  const resultvalidate = validationResult(req);
+  console.log(resultvalidate);
+  if (resultvalidate.errors.length > 0) {
+    ResHelper.RenderRes(res, false, resultvalidate.errors);
+    return;
+  }
+  if (req.cookies.token) {
+    const userinfo = req.user;
+    let user = await userModel.findById(userinfo._id);
+    if(user)
+    {
+      const bcrypt = require('bcrypt');
+        const match = await bcrypt.compare(req.body.password, user.password);
+      if(match)
+      {
+        ResHelper.RenderRes(res, false, "Vui lòng nhập password khác hiện tại");
+      }
+      else
+      {
+        user.password = req.body.password
+        await user.save();
+        ResHelper.RenderRes(res, true, "Doi password thanh cong");
+       
+      }
+    }
+  }else {
+    ResHelper.RenderRes(res, false, "URL khong hop le");
+  }
+});
+
 module.exports = router;
