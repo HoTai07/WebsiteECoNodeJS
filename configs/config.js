3a7cbf19a5781eb72905772fcfe50b92292a7b91
Truong <ltvtd54@gmail.com>
2024-04-06 08:38:31 +0700

    Done Login function

--- configs/config.js
+++ configs/config.js
@@ -1,4 +1,9 @@
 module.exports= {
     SECRET_KEY:'webecoSecrestKey',
-    EXPIRE_JWT:'1d'
+    EXPIRE_JWT:'1d',
+    hostName: 'localhost:3000',
+    Host: "sandbox.smtp.mailtrap.io",
+    Port: 2525,
+    Username: "1523a2f687ca1a",
+    Password: "cd2ce104426b80",
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var checkLogin = require('../middlewares/checklogin')
+var checkAuthorize = require('../middlewares/checkauthorize');
+var userModel = require('../model/user')
+var ResHelper = require('../helper/ResponseHelper');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', checkLogin, checkAuthorize("admin", "user"), async function (req, res, next) {
+  let users = await userModel.find({}).exec();
+  console.log(req.user);
+  ResHelper.RenderRes(res, true, users)
 });
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
 
 module.exports = router;
